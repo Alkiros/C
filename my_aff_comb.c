fa2ae9e1203c379608e157f00ee75a8e2e4baadb
Guillaume <guillaumetran97@gmail.com>
2015-09-27 01:20:08 +0200

    Update my_aff_comb.c

--- my_aff_comb.c
+++ my_aff_comb.c
@@ -1,11 +1,35 @@
-int a, b, c;
-for(a=0;a<=7;a++)
+#include <stdio.h>
+
+int     main()
 {
-    for(b=a+1;b<=8;b++)
+  int var1;
+  int var2;
+  int var3;
+  var1 = 0;
+  var2 = 0;
+  var3 = 0;
+
+
+  while(var1 <= 7)
     {
-        for(c=b+1;c<=9;c++)
+      var2 = var1 + 1;
+      while(var2 <= 8)
         {
-            printf("%d%d%d\n",a,b,c);
+          var3 = var2 + 1;
+          while(var3 <= 9)
+            {
+              if (var1 != 7 || var2 != 8 || var3 != 9)
+                {
+                  printf("%d%d%d, ", var1, var2, var3);
+                }
+              else printf("%d%d%d\n", var1, var2, var3);
+              var3++;
+            }
+          var2++;
         }
+        var1++;
     }
-}
+
+ }
+
+/** By tran_- **/
