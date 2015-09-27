6e9d12b7406de4e4ed21e0d1da9d922a7df1ecd0
Guillaume <guillaumetran97@gmail.com>
2015-09-27 22:15:08 +0200

    Update my_affcomb2.c

--- my_affcomb2.c
+++ my_affcomb2.c
@@ -1,3 +1,13 @@
+/*                                                                                                                                                                                            
+** my_affcomb.c for my_affcomb in /home/tran_-                                                                                                                                                
+**                                                                                                                                                                                            
+** Made by guillaume tran                                                                                                                                                                     
+** Login   <tran_-@epitech.net>                                                                                                                                                               
+**                                                                                                                                                                                            
+** Started on  Sun Sep 27 14:45:40 2015 guillaume tran                                                                                                                                        
+** Last update Sun Sep 27 14:45:41 2015 guillaume tran                                                                                                                                        
+*/
+
 void    my_putchar(char c)
 {
   write(1, &c, 1);
@@ -36,5 +46,3 @@ int     main()
       num1++;
     }
 }
-
-/** By tran_- **/
