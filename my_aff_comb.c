8806b0a5aa362afe7c5b60f82f4ee3557d868bb5
Guillaume <guillaumetran97@gmail.com>
2015-09-27 02:36:20 +0200

    Update my_aff_comb.c

--- my_aff_comb.c
+++ my_aff_comb.c
@@ -1,4 +1,19 @@
-#include <stdio.h>
+void    my_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void	display(int var1, int var2, int var3)
+{
+  my_putchar(var1 + 48);
+  my_putchar(var2 + 48);
+  my_putchar(var3 + 48);
+  if (!(var1 == 7 && var2 == 8 && var3 == 9))
+    {
+      my_putchar(',');
+      my_putchar(' ');
+    }
+}
 
 int     main()
 {
@@ -7,25 +22,23 @@ int     main()
   int var3;
   var1 = 0;
 
-  while(var1 <= 7)
+
+  while (var1 <= 7)
     {
       var2 = var1 + 1;
-      while(var2 <= 8)
+      while (var2 <= 8)
         {
           var3 = var2 + 1;
-          while(var3 <= 9)
+          while (var3 <= 9)
             {
-              if (var1 != 7 || var2 != 8 || var3 != 9)
-                {
-                  printf("%d%d%d, ", var1, var2, var3);
-                }
-              else printf("%d%d%d\n", var1, var2, var3);
+              display(var1, var2, var3);
               var3++;
             }
           var2++;
         }
         var1++;
     }
+
  }
 
 /** By tran_- **/
