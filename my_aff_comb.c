5fa10746b00620d5ce34e35babf69e1994f71768
Guillaume <guillaumetran97@gmail.com>
2015-09-27 22:14:22 +0200

    Update my_aff_comb.c

--- my_aff_comb.c
+++ my_aff_comb.c
@@ -1,9 +1,18 @@
+/*                                                                                                                                                                                            
+** my_affcomb.c for my_affcomb in /home/tran_-                                                                                                                                                
+**                                                                                                                                                                                            
+** Made by guillaume tran                                                                                                                                                                     
+** Login   <tran_-@epitech.net>                                                                                                                                                               
+**                                                                                                                                                                                            
+** Started on  Sun Sep 27 14:45:40 2015 guillaume tran                                                                                                                                        
+** Last update Sun Sep 27 14:45:41 2015 guillaume tran                                                                                                                                        
+*/
 void    my_putchar(char c)
 {
   write(1, &c, 1);
 }
 
-void	display(int var1, int var2, int var3)
+void    display(int var1, int var2, int var3)
 {
   my_putchar(var1 + 48);
   my_putchar(var2 + 48);
@@ -38,7 +47,4 @@ int     main()
         }
         var1++;
     }
-
- }
-
-/** By tran_- **/
+}
